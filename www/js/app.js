4b48521e774dfdd7675298f4fd512e57bb03f5e2
Ben Allfree <ben@benallfree.com>
2015-10-13 23:08:38 -0700

    Checkpoint

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,4 @@
+alert('connect');
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -19,25 +20,77 @@ angular.module('fastcast', ['ionic'])
 })
 
 
-.controller('FastCast', function($scope) {
-  alert('attach');
+.controller('FastCast', function($scope, $http) {
+  $scope.is_recording = false;
+  var src = null;
+  var mediaRec = null;
+  $scope.has_recording = false;
+
+
+  $scope.stop = function() {
+    mediaRec.stopRecord();
+    mediaRec.seekTo(0);
+    mediaRec.play();
+    $scope.is_recording = false;
+    $scope.has_recording = true;
+  };
+  
   $scope.record = function()
   {
-    var src = "myrecording.m4a";
-     var mediaRec = new Media(src,
-         // success callback
-         function() {
-             console.log("recordAudio():Audio Success");
-         },
-
-         // error callback
-         function(err) {
-             console.log("recordAudio():Audio Error: "+ err.code);
-             console.log(err);
-         });
-
-     // Record audio
-     mediaRec.startRecord();
-  }
+    src = "cdvfile://localhost/persistent/podcast.m4a";
+    $scope.is_recording = true;
+    mediaRec = new Media(src,
+      function() {
+        console.log("recordAudio():Audio Success");
+      },
+      function(err) {
+        $scope.is_recording = false;
+        console.log("recordAudio():Audio Error: "+ err.code);
+        console.log(err);
+      }
+    );
+    mediaRec.startRecord();
+  };
+  
+  $scope.publish = function() {
+    $http.get('http://api.fast-cast.net').then(function(response) {
+      var data = response.data;
+      ft = new FileTransfer(),
+      options = new FileUploadOptions();
+      options.fileName = src;
+      options.mimeType = "audio/mp4";
+      options.chunkedMode = false;
+      options.headers = {
+        'Connection': 'close'
+      };
+      options.params = {
+        "key": data.key,
+        "AWSAccessKeyId": data.access_key,
+        "acl": "public-read",
+        "policy": data.policy,
+        "signature": data.signature,
+        "Content-Type": "audio/mp4"
+      };
+      
+      console.log(options.params);
+      
+      ft.upload(src, "http://fastcast.s3.amazonaws.com/",
+        function (e) {
+          console.log('success', e);
+        },
+        function (e) {
+          console.log('failed', e, ft);
+        }, options
+      );
+    }, function(response) {
+      console.log('Error getting publishing contract', response);
+    });
+  };
 });
 
+if(typeof(Media)=='undefined')
+{
+  var Media = function() {
+    var startRecord = function() {};
+  };
+}
